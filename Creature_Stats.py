42931508ce745935bcfbeee88af822517eb76377
Zconj095 <96359501+Zconj095@users.noreply.github.com>
2022-12-23 13:32:41 -0500

    Major Changes Throughout The Pipeline
    Minor adjustments to folder locations.
    huge file edits and a ton of revisions between definitions. Data2, Knowledge2, Intelligence2... Added some new scripts as well

---
+++ Creature_Stats.py
@@ -0,0 +1,40 @@
+CREATURESTATS = [0, 1, 2, 3, 4, 5]
+ATTACKSTAT = "ATTACKSTAT"
+DEFENSESTAT = "DEFENSESTAT"
+SPECIALDEFENSESTAT = "SPECIALDEFENSESTAT"
+SPECIALATTACKSTAT = "SPECIALATTACKSTAT"
+SPEEDSTAT = "SPEEDSTAT"
+HPSTAT = "HPSTAT"
+speedstatname = CREATURESTATS[0]
+speedstatvalue = pokemondimensionvaluename = SPEEDSTAT
+speedstatname = speedstatvalue
+speedstat = speedstatname = speedstatvalue
+
+
+specialattackstatname = CREATURESTATS[1]
+specialattackstatvalue = creaturestatsname = SPECIALDEFENSESTAT
+specialattackstatname = specialattackstatvalue
+specialattackstat = specialattackstatname = specialattackstatvalue
+
+
+defensestatname = CREATURESTATS[2]
+defensestatvalue = creaturestatsname = DEFENSESTAT
+defensestatname = defensestatvalue
+defensestat = defensestatname = defensestatvalue
+
+
+specialdefensestatname = CREATURESTATS[3]
+specialdefensestatvalue = creaturestatsname = SPECIALATTACKSTAT
+specialdefensestatname = specialdefensestatvalue
+specialdefensestat = specialdefensestatname = specialdefensestatvalue
+
+
+attackstatname = CREATURESTATS[4]
+attackstatvalue = creaturestatsname = ATTACKSTAT
+attackstatname = attackstatvalue
+attackstat = attackstatname = attackstatvalue
+
+hpstatname = CREATURESTATS[0]
+hpstatvalue = pokemondimensionvaluename = HPSTAT
+hpstatname = hpstatvalue
+hpstat = hpstatname = hpstatvalue
---
+++ Working Example.py
@@ -0,0 +1,23 @@
+from Language_Early_Stage import *
+from Types_Of_Wavelengths import *
+from Language_Extension_001 import *
+from Types_Of_Dimension_Value import *
+from Types_Of_Seasons import *
+from Time_Of_Day import *
+from Types_Of_Time import *
+from Types_Of_Length import *
+from Types_Of_Calendar_Time import *
+from Numbers_One_To_OneThousand import *
+from Creature_Stats import *
+from CreatureConditions import * 
+from Pokemonarrayscript import *
+CREATE,NEW,CLASS;'/n'
+'/t',NEW,CLASS,SHALL,BE,GIVEN,NAME,"STATES_OF_CONSCIOUSNESS.CLASSES";
+'/t/t',CLASS,SHALL,CONTAIN,THE,NAME,FOR,FIVE,DIFFERENT,TYPES,FOR,THE,EXISTING,STATES,OF,CONSCIOUSNESS
+'/t/t',TYPE,ONE,IS,THE,"CONSCIOUSNESS"
+'/t/t',TYPE,TWO,IS,THE,"UNCONSCIOUSNESS"
+'/t/t',TYPE,THREE,IS,THE,"NONCONSCIOUSNESS"
+'/t/t',TYPE,FOUR,IS,THE,"SUBCONCSCIOUSNESS"
+'/t/t',TYPE,FIVE,IS,THE,"SUPERCONSCIOUSNESS"
+'/t',END,CLASS;
+END,COMMAND;
